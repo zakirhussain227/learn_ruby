0f1b92ac3d56cab194da36750cb9606ccd4e8722
Zakir <zakirawkum@gmail.com>
2017-05-10 21:07:57 +0500

    Wrote code to calculator.rb to pass tests, Wrote tests to calculator_spec.rb and passed it

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(x,y)
+  return x+y
+end
+def subtract(x,y)
+  return x-y
+end
+def sum(arr)
+  total = 0
+  i = 0
+  l = arr.length
+  while i < l do
+    total += arr[i]
+    i += 1
+  end
+  return total
+end
+def product(* numbers)
+  result = 1
+  numbers.each { |n| result = result * n }
+  result
+end
+def power(a,b)
+  a**b
+end
+def fact(n)
+  if n<= 1
+    1
+  else
+    n * fact( n - 1 )
+  end
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,21 +79,37 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(product(3,4)).to eq(12)
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+    expect(product(3,4,5)).to eq(60)
+  end
+
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+    expect(power(4,3)).to eq(64)
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+    expect(fact(0)).to eq(1)
+  end
+  it "computes the factorial of 1" do
+    expect(fact(1)).to eq(1)
+  end
+  it "computes the factorial of 2" do
+    expect(fact(2)).to eq(2)
+  end
+  it "computes the factorial of 5" do
+    expect(fact(5)).to eq(120)
+  end
+  it "computes the factorial of 10" do
+    expect(fact(10)).to eq(3628800)
+  end
 end
