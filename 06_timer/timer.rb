3fe7047ef1ed1d86ed111f5dc9b754f9659a82da
Zakir <zakirawkum@gmail.com>
2017-05-16 21:49:25 +0500

    wrote code to pass all tests

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @hours = 0
+    @minutes = 0
+    @seconds = 0
+  end
+  def time_string
+    @hours = @seconds / 3600
+    @minutes = (@seconds % 3600) / 60
+    @seconds = @seconds % 60
+    "#{padded(@hours)}:#{padded(@minutes)}:#{padded(@seconds)}"
+  end
+  def padded(number)
+    "#{"%02d" % number}"
+  end
 end
