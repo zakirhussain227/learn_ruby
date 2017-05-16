a0375447edad6e5baec63c183813dbf084255498
Zakir <zakirawkum@gmail.com>
2017-05-16 17:14:58 +0500

    all tests passed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+attr_accessor :title
+def title
+  t = @title.split(" ")
+  t.map!{ |t|
+    if t == "and" or t == "in" or t == "the" or t == "of" or t == "a" or t == "an"
+      t.downcase
+    else
+      t.capitalize
+    end
+  }
+  t = t.join(" ")
+  t[0] = t[0].upcase
+  t
+end
 end
